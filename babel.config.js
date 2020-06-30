cc238ebbddfc8c7d411159defddc9cdb00b02bdb
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-06-30 16:29:12 +0100

    update babel config to (attempt) handling of regenerator-runtime

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: {
+          version: 3.6,
+          proposals: true,
+        },
+      },
+    ],
     '@babel/preset-typescript',
     '@babel/preset-react',
     '@emotion/babel-preset-css-prop',
