84bf7e0b674aafd8564864a200d299473bee127a
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-06-30 16:50:36 +0100

    update babel.config attempt 2

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,19 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        corejs: {
-          version: 3.6,
-          proposals: true,
-        },
-      },
-    ],
+    // [
+    //   '@babel/preset-env',
+    //   {
+    //     useBuiltIns: 'usage',
+    //     corejs: {
+    //       version: 3.6,
+    //       proposals: true,
+    //     },
+    //   },
+    // ],
+    '@babel/preset-env',
     '@babel/preset-typescript',
     '@babel/preset-react',
     '@emotion/babel-preset-css-prop',
   ],
+  plugins: ['@babel/plugin-transform-runtime'],
 };
