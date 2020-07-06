d4da0228b3920715b0efcb7bc6d57965c6af4148
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-07-06 19:44:12 +0100

    adapt font size conventions

--- packages/theme/src/constants.ts
+++ packages/theme/src/constants.ts
@@ -6,12 +6,13 @@ export const font = {
       'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif',
   },
   size: {
-    small: 12,
-    body: 14,
-    subtitle: 16,
+    heading: 28,
+    subheading: 24,
+    title: 20,
     section: 18,
-    dek: 20,
-    title: 24,
+    subtitle: 16,
+    body: 14,
+    caption: 12,
   },
 };
 
