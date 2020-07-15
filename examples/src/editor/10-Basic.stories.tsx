6ecd6dc9195f3bf304db7ee39dde2695d76dc812
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-07-15 12:57:43 +0100

    demonstrate editor onChange

--- examples/src/editor/10-Basic.stories.tsx
+++ examples/src/editor/10-Basic.stories.tsx
@@ -4,7 +4,18 @@ import { Editor } from '@saruni-ui/editor';
 export default { title: 'Editor' };
 
 export const BasicEditor = (props) => {
-  return <Editor />;
+  const [state, setState] = React.useState({ output: null });
+
+  const handleChange = (props) => {
+    setState({ output: JSON.stringify(props.doc, null, 2) });
+  };
+
+  return (
+    <React.Fragment>
+      <Editor onChange={handleChange} />
+      {state.output && <pre>{state.output}</pre>}
+    </React.Fragment>
+  );
 };
 
 BasicEditor.story = {
